217e36ae043df3a465399e1e65c440ea32bc985e
C80609A <c080609a@gmail.com>
2017-04-28 08:42:46 +0300

    develop: controller: добавил методы для 'idle' и 'day_plan'.

--- app/controllers/chief_povar_controller.rb
+++ app/controllers/chief_povar_controller.rb
@@ -3,7 +3,12 @@ class ChiefPovarController < ApplicationController
   helper Front::ChiefPovar::FormsHelper
 
   # Шеф-повар попадает на экран с меню с кнопками "Готовить", "Списывать"
-  def idle_chief_povar
+  def idle
+
+  end
+
+  # Шеф-повар попадает на экран со списком "что приготовить сегодня в своём цеху"
+  def day_plan
 
   end
 
